1293efbf2e4863ab3cdb46ae1759aa9d4e5d0552
AlexCLeduc <ledualex@gmail.com>
2024-01-20 13:17:31 -0500

    pick unique name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="data-fetcher",
+    name="django-data-fetcher",
     version="0.0.1",
     author="AlexCLeduc",
     # author_email="author@example.com",
