518662178a220663543ba402f98ac7a9a7473787
Dmytro Litvinov <me@dmytrolitvinov.com>
2024-03-03 16:08:16 +0200

    Update setup.py (#3)
    Fix broken link to GitHub repo from PyPI

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     # description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AlexCleduc/data-fetcher",
+    url="https://github.com/AlexCLeduc/django-data-fetcher",
     packages=[
         # find_packages() also includes extraneous stuff, like testing and sample_app
         package
