0b53827b8d65f494c65e47b48e80a24eead09c86
AlexCLeduc <ledualex@gmail.com>
2024-03-30 11:47:17 -0400

    publish 2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-data-fetcher",
-    version="2.1.1",
+    version="2.2",
     author="AlexCLeduc",
     # author_email="author@example.com",
     # description="A small example package",
